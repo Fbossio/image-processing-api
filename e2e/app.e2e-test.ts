f126d394fe3bb107a777cdd0cf4417f625201347
Fbossio <felixbossio@gmail.com>
2023-09-14 22:15:59 -0500

    chore: remove docker files and change paths for e2e tests

--- e2e/app.e2e-test.ts
+++ e2e/app.e2e-test.ts
@@ -1,7 +1,13 @@
+import fs from 'fs'
 import request from 'supertest'
 import server from '../src'
 
-const TEST_IMAGE = '/app/src/tests/assets/test_images/encenadaport.jpg'
+const TEST_IMAGE =
+  './src/tests/assets/test_images/b9e2378f-8362-4b50-9733-cb7873f40fde.jpg'
+const SAVED_IMAGE_PATH =
+  './src/assets/full/b9e2378f-8362-4b50-9733-cb7873f40fde.jpg'
+const GENERATED_IMAGE_PATH =
+  './src/assets/thumb/300x200_b9e2378f-8362-4b50-9733-cb7873f40fde.jpg'
 
 describe('Images routes', () => {
   let consoleError: any
@@ -11,6 +17,12 @@ describe('Images routes', () => {
   })
   afterAll((done) => {
     server.close(done)
+    if (fs.existsSync(SAVED_IMAGE_PATH)) {
+      fs.unlinkSync(SAVED_IMAGE_PATH)
+    }
+    if (fs.existsSync(GENERATED_IMAGE_PATH)) {
+      fs.unlinkSync(GENERATED_IMAGE_PATH)
+    }
   })
   afterEach(() => {
     jest.restoreAllMocks()
@@ -28,7 +40,9 @@ describe('Images routes', () => {
   })
   it('should get a resized image', async () => {
     const response = await request(server)
-      .get('/api/v1/images/resize?name=encenadaport.jpg&width=300&height=200')
+      .get(
+        '/api/v1/images/resize?name=b9e2378f-8362-4b50-9733-cb7873f40fde.jpg&width=300&height=200',
+      )
       .expect(200)
     expect(response).toBeDefined()
   })
